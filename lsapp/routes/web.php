c69d2c702b689b3c049fc9c4fad7c7e59783e78b
Mihaylov,Petar P <p.mihaylov@student.fontys.nl>
2020-05-27 09:02:47 +0200

    models & database migrations

--- lsapp/routes/web.php
+++ lsapp/routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'PageController@index');
 Route::get('/about', 'PageController@about');
 Route::get('/services', 'PageController@services');
+
+Route::resource('posts', 'PostController');
