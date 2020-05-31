a5142b07ffaf32319ec7e9f9c9b377d57fa98231
Mihaylov,Petar P <p.mihaylov@student.fontys.nl>
2020-05-31 19:43:45 +0200

    user authentication

--- lsapp/app/Http/Controllers/PostController.php
+++ lsapp/app/Http/Controllers/PostController.php
@@ -54,6 +54,7 @@ class PostController extends Controller
         $post = new Post;
         $post->title = $request->input('title');
         $post->body = $request->input('body');
+        $post->user_id = auth()->user()->id;
 
         $post->save();
 
--- lsapp/resources/views/layouts/app.blade.php
+++ lsapp/resources/views/layouts/app.blade.php
@@ -1,22 +1,33 @@
-<!DOCTYPE html>
-<html lang="{{ config('app.locale') }}">
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
-    <title>{{ config('app.name', 'LSAPP') }}</title>
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
+    <div id="app">
 
-    @include('inc.navbar')
+        @include('inc.navbar')
 
-    <div class="container">
-        @include('inc.messages')
-        @yield('content')
+        <main class="py-4 container">
+            @include('inc.messages')
+            @yield('content')
+        </main>
     </div>
-
-
-    <script src="{{asset('js/app.js')}}"></script>
-
 </body>
 </html>
